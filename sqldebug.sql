7ca3695aa373bd49d9259519554e36f7deb3a000
Svein Samson <samson.svein@gmail.com>
2024-03-04 22:45:37 +0100

    Ajout d'une class pour gérer les API (WIP)

--- sqldebug.sql
+++
@@ -1,5 +0,0 @@
-
-2024-03-04 18:38:10 | CREATE TABLE `forum`.`user` (
-                `id` INT PRIMARY KEY AUTO_INCREMENT,
-            `mail` VARCHAR(255), `password` CHAR(60), `state` VARCHAR(255), `created_at` DATETIME, `creator` VARCHAR(255), `updated_at` DATETIME, `updator` VARCHAR(255)
-);
